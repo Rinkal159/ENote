e60d65db306d065f2165cd02d75eb81d746daefd
rinkal159 <rinkalsingapuri@gmail.com>
2026-05-19 15:02:31 +0530

    Tailwind configurations.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    darkMode: "class",
+    content: [
+        "./templates/**/*.html",
+        "./static/**/*.js"
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    DEFAULT: "#38bdf8"
+                }
+            }
+        }
+    },
+
+    plugins: [],
+};
